e20ad21f79d4a428b266e9d3715572203459d3f2
mvarbichki <116360901+mvarbichki@users.noreply.github.com>
2024-03-11 17:27:21 +0200

    Progresing over DB model structure

--- moviesapp/models.py
+++ moviesapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# TODO clarify movie model db
+
+class Movies(models.Model):
+    # Subclass enumeration types to define choices. Will be used in the front end as drop-down menu of choices
+    class Genres(models.TextChoices):
+        # The first value is stored in the DB and the second values is human-readable show in the drop-down
+        HORROR = "HR", "Horror"
+        THRILLER = "TR", "Thriller"
+        ROMANTIC = "RM", "Romantic"
+        ANIMATION = "AN", "Animation"
+        COMEDY = "CM", "Comedy"
+        FANTASY = "FN", "Fantasy"
+        ACTION = "AC", "Action/Adventure"
+        HISTORICAL = "HT", "Historical"
+        DRAMA = "DR", "Drama"
+        MYSTERY = "MT", "Mystery"
+        MUSICAL = "MC", "Musical"
+        CRIME = "CR", "Crime"
+
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=230)
+    release_year = models.PositiveSmallIntegerField()
+    director = models.CharField(max_length=53)
+    gener = models.CharField(max_length=20, choices=Genres.choices)
+    # TODO how to store the user_likes?
+    users_likes = ""
